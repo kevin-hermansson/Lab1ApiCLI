3d992d35e3c5964c7c513f36a6c61d60b528af91
Onehundra <hermanssonkevin@gmail.com>
2026-05-05 14:17:07 +0200

    Disable DbContext for now

--- Controllers/ProductController.cs
+++ Controllers/ProductController.cs
@@ -7,17 +7,17 @@ using Lab1ApiCLI.Data;
 [Route("api/[controller]")]
 public class ProductController : ControllerBase
 {
-    private readonly AppDbContext _context;
+    //private readonly AppDbContext _context;
 
-    public ProductController(AppDbContext context)
-    {
-        _context = context;
-    }
+    //public ProductController(AppDbContext context)
+    //{
+      //  _context = context;
+    //}
 
     [HttpGet]
     public async Task<ActionResult<IEnumerable<Product>>> GetProducts()
     {
-            return Ok(new List<Product>
+            return  Ok(new List<Product>
             {
                 new Product { Id = 1, Name = "Product 1", Price = 10.99m },
                 new Product { Id = 2, Name = "Product 2", Price = 19.99m },
@@ -29,8 +29,8 @@ public class ProductController : ControllerBase
     [HttpPost]
     public async Task<ActionResult<Product>> CreateProduct(Product product)
     {
-        _context.Products.Add(product);
-        await _context.SaveChangesAsync();
+        // _context.Products.Add(product);
+        // await _context.SaveChangesAsync();
 
         return Ok(product);
     }
