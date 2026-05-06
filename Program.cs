3bf7ecc54596bb0b9506c2f3cdffc6eeac722700
kevin-hermansson <hermanssonkevin@gmail.com>
2026-05-06 21:26:20 +0200

    fix cs db

--- Program.cs
+++ Program.cs
@@ -8,7 +8,8 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddDbContext<AppDbContext>(options =>
-    options.UseSqlServer(builder.Configuration["DefaultConnection"]));
+    options.UseSqlServer(
+        builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddSwaggerGen();
 builder.Services.AddApplicationInsightsTelemetry();
