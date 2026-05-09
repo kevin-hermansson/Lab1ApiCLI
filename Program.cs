8bac1076c2849fea15a900b9079bf9ebb5a04569
kevin-hermansson <hermanssonkevin@gmail.com>
2026-05-09 14:32:02 +0200

    fix key vault connection string

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddDbContext<AppDbContext>(options =>
     options.UseSqlServer(
-        builder.Configuration.GetConnectionString("DefaultConnection")));
+        builder.Configuration["DefaultConnection"]));
 
 builder.Services.AddSwaggerGen();
 builder.Services.AddApplicationInsightsTelemetry();
